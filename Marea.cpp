9070b7eca48067564c2c5b99c97bec431bda50a1
maarea55525 <165757394+maarea55525@users.noreply.github.com>
2024-04-27 08:44:06 +0700

    Add files via upload

---
+++ Marea.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+	int number1;
+	int number2;
+	int total;
+	cout<<"-----Calculator-----"<<endl;
+	cout<<"please enter 2 number to calculator:"<<endl;
+	cout<<"First Number =";
+	cin>>number1;
+	cout<<"Second Number =";
+	cin>>number2;
+	total=number1+number2;
+	cout<<"Total ="<<total;
+	
+	return 0;
+}
+
